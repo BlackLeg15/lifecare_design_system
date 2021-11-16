9b428a144b2cd881a91dce7455ca68814810c275
Adby Santos <adby.santos15@outlook.com>
2021-11-16 11:10:45 -0300

    fix: export custom scaffold

--- lib/lifecare_design_system.dart
+++ lib/lifecare_design_system.dart
@@ -2,3 +2,4 @@ library lifecare_design_system;
 
 export 'src/themes/light_theme.dart';
 export 'src/widgets/custom_button/custom_button.dart';
+export 'src/widgets/custom_scaffold/custom_scaffold.dart';
